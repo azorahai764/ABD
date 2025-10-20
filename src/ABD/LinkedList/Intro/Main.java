4ec6e44261b6834a42bd08cb759d5bb8aaaadf3f
azorahai764 <v764945@bbdnitm.ac.in>
2025-10-20 16:23:44 +0530

    My code

--- src/ABD/LinkedList/Intro/Main.java
+++ src/ABD/LinkedList/Intro/Main.java
@@ -36,6 +36,7 @@ public class Main {
             Tail.next = newnode;
             Tail = newnode;
         }
+
         public void addMid(int idx, int data){
             if(idx==0){
                 addFirst(data);
@@ -53,6 +54,56 @@ public class Main {
             temp.next=newnode;
         }
 
+        public int removeFirst(){
+            if(size==0){
+                System.out.println("Empty");
+                return Integer.MIN_VALUE;
+            } else if(size==1){
+                int val=Head.data;
+                Head=Tail=null;
+                size=0;
+                return val;
+            }
+            int val=Head.data;
+            Head=Head.next;
+            size--;
+            return val;
+        }
+
+        public int removeLast(){
+            if(size==0){
+                System.out.println("Empty");
+                return Integer.MIN_VALUE;
+            } else if(size==1){
+                int val=Head.data;
+                Head=Tail=null;
+                size=0;
+                return val;
+            }
+            Node prev=Head;
+            for (int i = 0; i < size-2; i++) {
+                prev=prev.next;
+            }
+            int val=Tail.data;
+            prev.next=null;
+            Tail=prev;
+            size--;
+            return val;
+        }
+
+        public int itrSearch(int key){
+            Node temp=Head;
+            int i=0;
+            while(temp!=null){
+                if(temp.data==key){
+                    return i;
+                }
+                temp=temp.next;
+                i++;
+            }
+            return -1;
+        }
+
         public void printList() {
             if (Head == null) {
                 System.out.println("Empty");
@@ -66,7 +117,6 @@ public class Main {
             System.out.print("null \n");
         }
 
-
     public static void main(String[] args) {
         Main l=new Main();
         l.addFirst(2);
@@ -76,5 +126,12 @@ public class Main {
         l.addMid(2,3);
         l.printList();
         System.out.println("Size: "+l.size);
+        l.removeFirst();
+        l.printList();
+        System.out.println("Size: "+l.size);
+        l.removeLast();
+        l.printList();
+        System.out.println("Size: "+l.size);
+        System.out.println(l.itrSearch(3));
     }
 }
